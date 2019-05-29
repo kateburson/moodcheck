c54b39549b530f96a78aec4fed56dafaef32d9cb
Kate Burson <burson.kate@gmail.com>
2019-05-29 11:44:37 -0700

    working on re-charting on radio change

--- client/src/pages/charts.js
+++ client/src/pages/charts.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Line } from 'react-chartjs-2';
-import moment from "moment";
+import { Line } from "react-chartjs-2";
+import moment, { weekdaysMin } from "moment";
+import { RadioGroup } from "react-materialize";
 
 import API from "../utils/API";
 import Nav from "../components/sideNav";
@@ -8,24 +9,50 @@ import Nav from "../components/sideNav";
 class Charts extends React.Component {
 
   state = {
-    moods: [],
-    highs: [],
-    lows: [],
-    labels: [], 
-    chartData: {}
+    chartData: {},
+    radio: "0",
+    range: [{
+      highs: [],
+      lows: [],
+      labels: []
+    },
+    {
+      highs: [],
+      lows: [],
+      labels: []
+    },
+    {
+      highs: [],
+      lows: [],
+      labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    }]
   }
 
   componentWillMount = () => {
     const id = localStorage.getItem("id");
     API.findMoods(id)
     .then(res => {
-      res.data.mood.map(mood => this.state.highs.push(Number(mood.high)));
-      res.data.mood.map(mood => this.state.lows.push(Number(mood.low)));
-      const weekdays =  ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-      res.data.mood.map(mood => this.state.labels.push(weekdays[moment(mood.date).day()]))
-    }).then(() => {
-      const data = {
-      labels : this.state.labels,
+    const lastWeek = moment().startOf('day').subtract(1,'week');
+    const lastMonth = moment().startOf('day').subtract(1,'month');
+    const lastYear = moment().startOf('day').subtract(1,'year');
+    var yesterdayEndOfRange =  moment().endOf('day').subtract(1,'day');
+    const week = res.data.mood.filter(mood => moment(mood.date).isBetween(lastWeek, yesterdayEndOfRange))
+    week.map(day => this.state.range[0].highs.push(day.high));
+    week.map(day => this.state.range[0].lows.push(day.low));
+    const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    week.map(day => this.state.range[0].labels.push(weekdays[moment(day.date).day()]));
+    const month = res.data.mood.filter(mood => moment(mood.date).isBetween(lastMonth, yesterdayEndOfRange));
+    month.map(day => this.state.range[1].highs.push(day.high));
+    month.map(day => this.state.range[1].lows.push(day.low));
+    const year = res.data.mood.filter(mood => moment(mood.date).isBetween(lastYear, yesterdayEndOfRange));
+    year.map(day => this.state.range[2].highs.push(day.high));
+    year.map(day => this.state.range[2].lows.push(day.low));
+    }).then(() => {this.buildChart()})
+  }
+
+  buildChart = () => {
+    const data = {
+      labels : this.state.range[Number(this.state.radio)].labels,
       datasets: [
         {
           label: "High",
@@ -35,7 +62,7 @@ class Charts extends React.Component {
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
           pointHighlightStroke: "rgba(220,220,220,1)",
-          data: this.state.highs
+          data: this.state.range[Number(this.state.radio)].highs
         },
         {
           label: "Low",
@@ -45,19 +72,33 @@ class Charts extends React.Component {
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
           pointHighlightStroke: "rgba(151,187,205,1)",
-          data: this.state.lows
+          data: this.state.range[Number(this.state.radio)].lows
         }
       ]
     }
     this.setState({chartData: data})
     console.log(this.state.chartData)
-    });
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({ radio: e.target.value });
+    this.buildChart();
   }
   
   render() {
     return(
       <div>
         <Nav />
+        <div>
+          <RadioGroup
+            name="dateRange"
+            label="Date Range"
+            value={this.state.radio}
+            options={[{label: "week", value: "0"}, {label: "month", value: "1"}, {label: "year", value: "2"}]}
+            onChange={this.handleChange}
+          />
+        </div>
         <Line data={this.state.chartData} />
       </div>
     )
