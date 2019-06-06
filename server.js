99e74e1caebc34b34f164aa6896dae4a612f3ac1
Kate Burson <burson.kate@gmail.com>
2019-06-06 08:12:20 -0700

    took out new proxy middleware

--- server.js
+++ server.js
@@ -5,26 +5,6 @@ const mongoURI = require("./config/keys");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3002;
-const httpProxy = require('http-proxy-middleware');
-
-app.use('/api', httpProxy({
-  logLevel     : 'debug',
-  target       : API_SERVER,
-  changeOrigin : true,
-  secure       : false,
-  xfwd         : true,
-  router: {
-      '/api/auth/login': `${API_SERVER}/auth/login`
-  },
-  onProxyReq   : function (proxyReq, req, res) {
-      // Browers may send Origin headers even with same-origin
-      // requests. To prevent CORS issues, we have to change
-      // the Origin to match the target URL.
-      if (proxyReq.getHeader('origin')) {
-          proxyReq.setHeader('origin', API_SERVER);
-      }
-  }
-}));
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
