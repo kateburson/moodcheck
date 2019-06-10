26f26d11dce9748da6d7ed3656ac5630908be408
Kate Burson <burson.kate@gmail.com>
2019-06-10 13:46:38 -0700

    change user routes in utils/API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default {
   register: function(userData) {
-    return axios.post("/api/users/register", userData)
+    return axios.post("/api/register", userData)
   },
   login: function(userData) {
-    return axios.post("/api/users/login", userData)
+    return axios.post("/api/login", userData)
   },
   validateToken: function(t) {
-    return axios.post("/api/users/validate", { token: t })
+    return axios.post("/api/validate", { token: t })
   },
   newEntry: function(entry, id) {
     return axios.post("/api/journal/" + id, entry)
