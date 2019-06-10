57cd0be4246e3d466692043b47a25adba38d1a9e
Kate Burson <burson.kate@gmail.com>
2019-06-10 13:34:59 -0700

    added heroku url to proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy('/api', { target: 'http://localhost:3002/'}));
+  app.use(proxy('/api', { target: "http://localhost:3002/" || "https://peaceful-wildwood-71736.herokuapp.com/"}));
 };
