6c2113c9f39343dff571cec398cb14758d80dd42
Kate Burson <burson.kate@gmail.com>
2019-06-15 10:56:26 -0700

    changed login route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,7 @@ export default {
     return axios.post("/api/users/register", userData)
   },
   login: function(userData) {
-    return axios.post("/api/users/login", userData)
+    return axios.post("/api/login", userData)
   },
   validateToken: function(t) {
     return axios.post("/api/users/validate", { token: t })
